25636b3e6500c9682e609c084f0a0642fcf9e2cc
cazmaar <kazeem.alatishe@yahoo.com>
2021-12-03 11:57:16 +0000

    pop up window

--- main.js
+++ main.js
@@ -4,10 +4,25 @@ let inputField = document.querySelector("#recipe-input-field");
 const recipesRenderBox = document.querySelector("#recipes-render-box");
 let recipesArray = [];
 
+function selectRecipeCards() {
+  let recipeCardArray = document.querySelectorAll(".recipe-card");
+  recipeCardArray.forEach(function (card) {
+    card.addEventListener("click", showModal);
+  });
+}
+
+function showModal(event) {
+  let backgroundModal = document.createElement("div");
+  backgroundModal.classList.add("background-Modal");
+  let documentHeight = document.body.clientHeight;
+  backgroundModal.style.height = `${documentHeight}px`;
+  document.body.appendChild(backgroundModal);
+}
+
 function handleRecipeClick() {
   let optionPicked = inputField.value;
-  recipesArray = [];
   fetchRecipe(optionPicked);
+  recipesArray = [];
 }
 
 function handleInputChange() {}
@@ -21,25 +36,30 @@ async function fetchRecipe(food) {
         let recipeObject = {
           image: hit.recipe.image,
           name: hit.recipe.label,
+          mealType: hit.recipe.mealType,
+          ingredients: hit.recipe.ingredientLines,
         };
+        console.log(recipeObject);
         recipesArray.push(recipeObject);
       });
     });
-    renderRecipes(recipesArray);
+  renderRecipes(recipesArray);
+  selectRecipeCards();
 }
 
 function renderRecipes(array) {
   recipesRenderBox.innerText = "";
-  array.forEach(function(recipe) {
+  array.forEach(function (recipe) {
     let recipeCardDiv = document.createElement("div");
+    recipeCardDiv.classList.add("recipe-card");
     let recipeImg = document.createElement("img");
     let recipeHeader = document.createElement("h4");
-    recipeImg.setAttribute("src", recipe.image)
-    recipeHeader.innerText = recipe.name
+    recipeImg.setAttribute("src", recipe.image);
+    recipeHeader.innerText = recipe.name;
     recipeCardDiv.appendChild(recipeImg);
     recipeCardDiv.appendChild(recipeHeader);
     recipesRenderBox.appendChild(recipeCardDiv);
-  })
+  });
 }
 
 button.addEventListener("click", handleRecipeClick);
