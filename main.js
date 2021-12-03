03a942e59bd131717958a1350c641c50356ff05e
valeriocipolla <valeriocipolla92@gmail.com>
2021-12-03 09:57:42 +0000

    working fetch request

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
+const API_KEY = "080ad8052cf223a7d33ffc8dd6683d03";
+
 function handleRecipeClick() {}
 
 function handleInputChange() {}
 
-async function fetchRecipe(food) {}
+async function fetchRecipe(food) {
+  await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${food}&app_id=0c94e0cd&app_key=${API_KEY}
+  `)
+    .then((response) => response.json())
+    .then((data) => data.hits.forEach(hit => console.log(hit.recipe.cuisineType)));
+}
+
+
+fetchRecipe("american")
